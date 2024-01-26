e2e770578888432650aa59766f58dfd8997e74c8
chetanis <anis.chetouane@etu.usthb.dz>
2024-01-26 17:05:17 +0100

    adding attributes to doctor

--- SbitarApp/models.py
+++ SbitarApp/models.py
@@ -6,6 +6,10 @@ class Doctor(models.Model):
     name = models.CharField(max_length=255,null=True)
     last_name = models.CharField(max_length=255,null=True)
     specialty = models.CharField(max_length=255,null=True)
+    experience = models.CharField(max_length=3,null=True)  
+    email = models.CharField(max_length=50,null=True)  
+    gender = models.CharField(max_length=15,null=True)  
     phone_number = models.CharField(max_length=15,null=True)  
+    dob = models.DateField(null=True) 
 
 
--- SbitarApp/views.py
+++ SbitarApp/views.py
@@ -1,4 +1,5 @@
 import os
+from django.contrib import messages
 from django.shortcuts import render
 import logging
 from .forms import DoctorForm
@@ -25,33 +26,21 @@ def homePage(request):
     
 def addDoctor(request):
     if request.method == 'POST':
-        logging.debug("POST POST POST POST POST POST POST v ")
         form = DoctorForm(request.POST)
         if form.is_valid():
             form.save()
             form = DoctorForm()
-        logging.debug(request.POST)
+            messages.success(request, 'Doctor added successfully!')
+        else:
+            messages.error(request, 'Form submission error. Please correct the errors below.')
         template_path ='/Users/anischetouane/Desktop/projet si/attempt 2/sbitar/SbitarApp/templates/add-doctor.html'
         return render(request, template_path, {'form': form})
     else:
-        logging.debug("get get get gte gtegte gtegte")
         context = {}
         template_path ='/Users/anischetouane/Desktop/projet si/attempt 2/sbitar/SbitarApp/templates/add-doctor.html'
         return render(request, template_path, context)
 
 
-# def add_doctor(request):
-#     logging.debug("Form submitted successfully!")
-#     if request.method == 'POST':
-#         form = DoctorForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             form = DoctorForm()
-#             template_path ='/Users/anischetouane/Desktop/projet si/attempt 2/sbitar/SbitarApp/templates/add-doctor.html'
-#             return render(request, template_path, {'form': form})
-#     else:
-#         form = DoctorForm()
-#         template_path ='/Users/anischetouane/Desktop/projet si/attempt 2/sbitar/SbitarApp/templates/add-doctor.html'
-#     return render(request, template_path, {'form': form})
+
 
 
