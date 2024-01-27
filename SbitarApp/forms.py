2a0486bdd9c70101956c69cb81074d5b3cd7ada3
chetanis <anis.chetouane@etu.usthb.dz>
2024-01-27 21:47:03 +0100

    edited doctor module

--- SbitarApp/forms.py
+++ SbitarApp/forms.py
@@ -5,7 +5,7 @@ from .models import Doctor, Patient,Appointment, RoomAllotment
 class DoctorForm(forms.ModelForm):
     class Meta:
         model = Doctor
-        fields = ['name', 'last_name', 'specialty', 'experience', 'email', 'gender', 'phone_number', 'dob']
+        fields = ['name', 'last_name', 'departement', 'title', 'email', 'gender', 'phone_number', 'dob']
 
 class PatientForm(forms.ModelForm):
     class Meta:
